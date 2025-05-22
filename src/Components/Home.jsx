51d07c3977fa7584e64663e13a8578c69d3607cf
Abir-Hasan-52 <abirhasan5208+github@gmail.com>
2025-05-23 00:37:29 +0600

    hero section added

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
- 
-import TopRecipes from './TopRecipes';
+import React from "react";
+
+import TopRecipes from "./TopRecipes";
+import Hero from "./Hero";
 
 const Home = () => {
-     
-    return (
-        
-        <div>
-             <TopRecipes></TopRecipes>
-        </div>
-    );
+  return (
+    <div>
+      <section className="my-4">
+        <Hero></Hero>
+      </section>
+      <section>
+        <h2 className="text-4xl text-center font-bold">Top Recipes</h2>
+        <p className="text-center text-gray-400 py-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum dolorem reprehenderit pariatur quia voluptas laudantium at quos est, quod placeat, dolor consectetur amet, cum facere labore doloribus. Repudiandae, dolorem suscipit.</p>
+         <TopRecipes></TopRecipes>
+      </section>
+    </div>
+  );
 };
 
 export default Home;
--- src/LayOuts/MainLayOut.jsx
+++ src/LayOuts/MainLayOut.jsx
@@ -1,22 +1,24 @@
-import React from 'react';
-import { Outlet } from 'react-router';
-import Navbar from '../Components/Navbar';
-import Footer from '../Components/Footer';
+import React from "react";
+import { Outlet } from "react-router";
+import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
+import Hero from "../Components/Hero";
 
 const MainLayOut = () => {
-    return (
-        <div>
-            <header>
-                <Navbar></Navbar>
-            </header>
-             <div className='w-11/12 mx-auto'>
-                <Outlet></Outlet>
-             </div>
-             <footer>
-                <Footer></Footer>
-             </footer>
-        </div>
-    );
+  return (
+    <div>
+      <header>
+        <Navbar></Navbar>
+         
+      </header>
+      <div className="w-11/12 mx-auto">
+        <Outlet></Outlet>
+      </div>
+      <footer>
+        <Footer></Footer>
+      </footer>
+    </div>
+  );
 };
 
 export default MainLayOut;
